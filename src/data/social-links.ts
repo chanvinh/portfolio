97274bbba4e4c4063c24b09c474185a618aab093
Vinh <chanvinh41@gmail.com>
2026-08-07 11:18:03 +0700

    Update linkedin

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -5,7 +5,7 @@ export const socialLinks: SocialLink[] = [
   { label: "GitHub", href: "https://github.com/chanvinh", icon: "FaGithub" },
   {
     label: "LinkedIn",
-    href: "https://www.linkedin.com/in/ch%E1%BA%A5n-vinh-26b930215/",
+    href: "https://www.linkedin.com/in/ung-ch%E1%BA%A5n-vinh-26b930215/",
     icon: "FaLinkedin",
   },
   { label: "Email", href: `mailto:${profile.email}`, icon: "HiMail" },
